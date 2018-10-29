81dc8111bebfce31ee358e1fae55280c977a2a22
pleasecheckhere2016 <707748808@qq.com>
2018-10-29 18:16:13 +0800

    +maven package

--- zhaodanmu-douyu-server/src/main/java/com/zhaodanmu/douyu/server/Main.java
+++ zhaodanmu-douyu-server/src/main/java/com/zhaodanmu/douyu/server/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        System.setProperty("io.netty.noUnsafe","false");
         PropertiesUtil.load("douyu.properties");
         String roomsArray = PropertiesUtil.getValue("rooms").trim();
         String[] rooms = roomsArray.split(",");
